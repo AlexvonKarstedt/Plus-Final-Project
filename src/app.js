732338e10618ca409d57ad7eebe7398c0f6745b6
AlexvonKarstedt <AlexvonKarstedt@gmail.com>
2021-03-25 04:34:19 -0400

    Created JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
-let apiKey = "d38b3fbab5d2bec8684d5a27e2c576ad";
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let seconds = date.getSeconds();
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}:${seconds}`;
+}
+
+function showTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityEllement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityEllement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+let apiKey = "cbb7dffbf17166ca321c36350499d9ef";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Barrie&appid=${apiKey}&units=metric`;
+console.log(apiKey);
+console.log(apiUrl);
+
+axios.get(apiUrl).then(showTemperature);
